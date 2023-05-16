c0847dffcacfa8a0d25eee94610d8e8a4acb3075
ronijaat <111105752+ronijaat@users.noreply.github.com>
2023-05-16 23:35:31 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose');
-
-
-
-
-
-mongoose.connect('mongodb://127.0.0.1:27017/csvUploads');
-
+const mongoose = require ('mongoose');
+//const dotenv=require('dotenv')
+//dotenv.config({path: 'config.env'})
+//mongoose.connect("mongodb+srv://Ravi:COtJieb3AxMIo9AD@cluster0.qnb48ii.mongodb.net/")
+mongoose.connect('mongodb+srv://Ravi:COtJieb3AxMIo9AD@cluster0.qnb48ii.mongodb.net/',  { 
+       useNewUrlParser: true,
+       //useFindAndModify: false,
+       useUnifiedTopology: true
+  });
 const db = mongoose.connection;
 
-db.once('open', function(){
-    console.log('database connected to the server successfully!');
+db.on('error',console.error.bind(console,"Error connecting to Mongodb"));
+
+db.once('open',function(){
+    console.log("Successfully connect to the database")
 });
 
-module.exports = db;
+module.exports=db;
